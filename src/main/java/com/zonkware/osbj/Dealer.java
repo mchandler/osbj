9f2c1b00fc6cd5e10e489b81c756b3f851eb906a
Mike Chandler <mike@rmwebfx.com>
2014-06-05 15:38:32 -0700

    Adding dealer game conclusion steps

--- src/main/java/com/zonkware/osbj/Dealer.java
+++ src/main/java/com/zonkware/osbj/Dealer.java
@@ -1,5 +1,41 @@
 package com.zonkware.osbj;
 
 public class Dealer extends Player {
-
+	
+	private boolean willTakeCard;
+	
+	@Override
+	public void take(Card card) {
+		super.take(card);
+		
+		if (willTakeCard == true) {
+			willTakeCard = false;
+		}
+	}
+	
+	public boolean concludeByStep(Player player) {
+		if (player.beats(this)) {
+			willTakeCard = true;
+		}
+		
+		if (player.totalHand() == totalHand()) {
+			return processPushDecisions();
+		}
+		
+		if (this.beats(player)) {
+			stand();
+		}
+		
+		return isStanding();
+	}
+	
+	private boolean processPushDecisions() {
+		if (totalHand() >= 16) {
+			stand();
+		} else {
+			willTakeCard = true;
+		}
+		
+		return isStanding();
+	}
 }
